bfba203915f32b954be9a762e7063b65587a24f5
Victor Diaz <victormdb@gmail.com>
2015-03-25 18:40:30 +0100

    bluetooth example updated

--- PHONK-examples/included_examples/Bluetooth/main.js
+++ PHONK-examples/included_examples/Bluetooth/main.js
@@ -6,42 +6,33 @@
 * https://learn.sparkfun.com/tutorials/using-the-bluesmirf/all
 */
 
-var txt = ui.addText("", 10, 450, ui.screenWidth, -1);
+var txt = ui.addText(10, 450, ui.screenWidth, -1);
 
-ui.addButton("Scan bluetooth", 10, 0, function() {
-    network.scanBluetoothNetworks(function(n, m, s) { 
+ui.addButton("Scan bluetooth", 10, 0).onClick(function() {
+    network.bluetooth.scanNetworks(function(n, m, s) { 
         console.log("hola", n, m, s);
         txt.append(n + " " + m + " " + s + "\n");
     });
-})
+});
 
-ui.addButton("Connect to bluetooth", 10, 150, function() {
-    network.connectBluetoothSerialByUi(function(m, data) {
+var btClient;
+ui.addButton("Connect to bluetooth", 10, 150).onClick(function() {
+    //if you want to use the Bluetooth Address, use 
+    //network.bluetooth.connectSerial(macAddess, function(status) {});
+    btClient = network.bluetooth.connectSerial(function(status) {
+        console.log("connected " + status);
+    });
+    
+    btClient.onNewData(function(data) {
         txt.text(data + "\n");
     });
-})
+});
 
-ui.addButton("Disconnect", 380, 150, function() {
-    network.disconnectBluetooth();
-})
+ui.addButton("Disconnect", 380, 150).onClick(function() {
+    btClient.disconnect();
+});
 
 var input = ui.addInput("message", 10, 300, 200, 100);
-var send = ui.addButton("Send", 210, 300, 150, 100, function() {
-    network.sendBluetoothSerial(input.getText() + "\n");
-})
-
-
-
-
-// ---------- other methods 
-// network.connectBluetoothSerialByMac("98:D3:31:30:1A:4E", function(m, data) {
-//     console.log(data);
-// });
-
-// network.connectBluetoothSerialByName("bqZUM_BT328", function(m, data) {
-//     console.log(m, data);
-// });
-// network.enableBluetooth(true);
-// network.enableBluetooth(false);
-// network.isBluetoothConnected(); 
-// network.getBluetoothBondedDevices();
+var send = ui.addButton("Send", 210, 300, 150, 100).onClick(function() {
+    btClient.send(input.getText() + "\n");
+});
