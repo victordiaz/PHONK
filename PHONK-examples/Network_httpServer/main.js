e9f9650237e49b9d0e06e047b7ae84363f95d8be
Victor Diaz <victormdb@gmail.com>
2015-03-25 17:55:07 +0100

    http server updated

--- PHONK-examples/Network_httpServer/main.js
+++ PHONK-examples/Network_httpServer/main.js
@@ -1,18 +1,25 @@
 /*
-*	Description SimpleHttpServer 
+*	SimpleHttpServer 
 */
 
-var server = network.startSimpleHttpServer(1111, function(url, method, header, parms, files) {
+var server = network.createSimpleHttpServer(1111);
+
+
+server.onNewRequest(function(url, method, header, parms, files) {
     console.log(url, method, header);
     
     //serves pure text to a given GET command
     if (url == "/helloworld") {
-        console.log("got helloworld get petition!");
-        return server.respond("hello world back!");
-    
+        console.log("got helloworld GET petition!");
+        
+        //if you want to do some UI call you must use as follows 
+        app.runOnUiThread(function() {
+             ui.toast("this shows on the ui!", 200); 
+        });
+        
+        return server.response("hello world back!");
     //return files in the current project folder
     } else {
         return server.serveFile(url, header);;
     }
-    
 });
