1e938cb7815455eb9db2546d7bf99a64961c20eb
Victor Diaz <victormdb@gmail.com>
2019-06-27 00:28:49 +0200

    better matrix sizing

--- PHONK-examples/examples/Graphical User Interface/Extra Views/main.js
+++ PHONK-examples/examples/Graphical User Interface/Extra Views/main.js
@@ -13,7 +13,7 @@ var knob = ui.addKnob(0.7, 0.55, 0.2, 0.2).onChange(function (o) {
   console.log(o.value)
 })
 
-var matrix = ui.addMatrix(0.05, 0.7, 0.45, 0.25, 10, 10).onChange(function (o) {
+var matrix = ui.addMatrix(0.05, 0.7, '0.45w', '0.45w', 10, 10).onChange(function (o) {
   console.log(o.x, o.y, o.value)
 })
 matrix.selectColumn(2)
