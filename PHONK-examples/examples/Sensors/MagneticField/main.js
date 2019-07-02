299679e36d54d9597947916c951f928590c51a68
Victor Diaz <victormdb@gmail.com>
2019-07-02 16:24:51 +0200

    new sensor examples

--- PHONK-examples/examples/Sensors/MagneticField/main.js
+++ PHONK-examples/examples/Sensors/MagneticField/main.js
@@ -8,7 +8,7 @@ var plotX = ui.addPlot(0.1, 0.15, 0.8, 0.1).name('x')
 var plotY = ui.addPlot(0.1, 0.30, 0.8, 0.1).name('y')
 var plotZ = ui.addPlot(0.1, 0.45, 0.8, 0.1).name('z')
 
-sensors.magnetic.onChange(function (data) {
+sensors.magneticField.onChange(function (data) {
   plotX.update(data.x)
   plotY.update(data.y)
   plotZ.update(data.z)
@@ -16,6 +16,6 @@ sensors.magnetic.onChange(function (data) {
 
 // start / stop magnetic sensor
 ui.addToggle('ON', 0.1, 0.6, 0.2, 0.1).onChange(function (o) {
-  if (o.checked) sensors.magnetic.start()
-  else sensors.magnetic.stop()
+  if (o.checked) sensors.magneticField.start()
+  else sensors.magneticField.stop()
 })
---
+++ PHONK-examples/examples/Sensors/RotationVector/main.js
@@ -0,0 +1,21 @@
+/*
+ * Phonk Example: Rotation Vector
+ */
+
+ui.addTitle(app.name)
+
+var plotX = ui.addPlot(0.1, 0.15, 0.8, 0.1).name('x').range(-1, 1)
+var plotY = ui.addPlot(0.1, 0.30, 0.8, 0.1).name('y').range(-1, 1)
+var plotZ = ui.addPlot(0.1, 0.45, 0.8, 0.1).name('z').range(-1, 1)
+
+sensors.rotationVector.onChange(function (data) {
+  plotX.update(data.x)
+  plotY.update(data.y)
+  plotZ.update(data.z)
+})
+
+// start / stop rotation vector
+ui.addToggle('ON', 0.1, 0.6, 0.2, 0.1).onChange(function (o) {
+  if (o.checked) sensors.rotationVector.start()
+  else sensors.rotationVector.stop()
+})
