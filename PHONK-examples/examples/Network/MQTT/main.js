8ed75df0e827a9726f5dbf364f39e69a536c14ce
Victor Diaz <victormdb@gmail.com>
2020-06-17 17:17:17 +0200

    update mqtt example to match MQTT eclipse paho library

--- PHONK-examples/examples/Network/MQTT/main.js
+++ PHONK-examples/examples/Network/MQTT/main.js
@@ -5,11 +5,12 @@
 ui.addTitle(app.name)
 ui.addSubtitle('MQTT client example. Edit the values to connect to your server.')
 
-var clientId = 'phonk'
-var host = '192.168.43.172'
-var port = 1883
-var username = null
-var password = null
+var connectionData = {
+  clientId: 'phonk',
+  broker: 'tcp://192.168.10.100:1883',
+  username: null,
+  password: null
+}
 
 var c = network.createMQTTClient()
 c.onNewData(function (e) {
@@ -17,10 +18,10 @@ c.onNewData(function (e) {
 })
 
 ui.addButton('connect and subscribe', 0.1, 0.45, 0.35, 0.1).onClick(function (e) {
-  c.connect(clientId, host, port, username, password)
+  c.connect(connectionData)
   c.subscribe('debug')
 })
 
 ui.addButton('publish', 0.55, 0.45, 0.35, 0.1).onClick(function (e) {
-  c.publish('debug', 'cuakcuak')
+  c.publish('debug', 'hello world', 2, false) // params: topic, data, qos, retain
 })
