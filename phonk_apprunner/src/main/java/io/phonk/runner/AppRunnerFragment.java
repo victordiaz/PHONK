e1f5267eac09d77b14d2aee3154024df10a38d91
Victor Diaz <victormdb@gmail.com>
2020-07-04 12:05:11 +0200

    disabling filewatcher for real :P

--- phonk_apprunner/src/main/java/io/phonk/runner/AppRunnerFragment.java
+++ phonk_apprunner/src/main/java/io/phonk/runner/AppRunnerFragment.java
@@ -171,7 +171,7 @@ public class AppRunnerFragment extends Fragment {
         super.onDestroy();
         MLog.d(TAG, "onDestroy");
         if (mAppRunner.interp != null) mAppRunner.interp.callJsFunction("onDestroy");
-        fileObserver.stopWatching();
+        // fileObserver.stopWatching();
         mAppRunner.byebye();
     }
 
