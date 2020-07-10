75c531a904a8ab3fa9163d2bf4edb3595b47ede6
Victor Diaz <victormdb@gmail.com>
2020-07-10 20:58:16 +0200

    update voice recognizer subtitle

--- PHONK-examples/examples/Media/VoiceRecognition/main.js
+++ PHONK-examples/examples/Media/VoiceRecognition/main.js
@@ -5,8 +5,8 @@
 
 ui.addTitle(app.name)
 ui.addSubtitle('Using the Android built-in voice recognizer\n' +
-              'it will show and play back the recognized text' +
-              'You might have internet connection on certain Android devices')
+              'it will recognize what you say it and play it back.\n' +
+              'The voice recognizer depends on your Android vendor (Google, Huawei, Samsung...) and for some you might have internet connection.')
 
 ui.addButton('Press to start the recognizer', 0.1, 0.35, 0.8, 0.4).onClick(function (e) {
   media.startVoiceRecognition(function (e) {
