61a694b08e498d6b857e833bab88034e34c3cc2e
Victor Diaz <victormdb@gmail.com>
2020-08-27 21:08:20 +0200

    webview example shows how to access PHONK's api #10

--- PHONK-examples/examples/Graphical User Interface/Webview/main.js
+++ PHONK-examples/examples/Graphical User Interface/Webview/main.js
@@ -6,21 +6,30 @@
 
 ui.addTitle(app.name)
 
-// load the content
+// load inlined content
 var webview = ui.addWebView(0.1, 0.1, 0.8, 0.25)
-// html content to load
-var content = '\
-  <html> \
-    <body style = "background: white">\
-      <h1>Inline HTML</h1>\
-      <p>lalallalalalal</p>\
-    </body>\
-  </html>'
-webview.loadData(content)
 
-// load html file
+function test () {
+  device.vibrate(100)
+  ui.toast('Hi!')
+}
+
+var inlineContent = '\
+  <html> \
+    <script> \
+    function t() { console.log(\'cuakcuak\') } \
+    </script> \
+    <body style = "background: white"> \
+      <button onclick="app.eval(\'device.vibrate(100)\');">vibrate</button> \
+      <button onclick="app.eval(\'test()\');">test()</button> \
+    </body> \
+  </html>'
+webview.loadData(inlineContent)
+
+// load html file with transparent background
 var webview2 = ui.addWebView(0.1, 0.4, 0.8, 0.25)
 webview2.loadFile('hello.html')
+webview2.props.background = '#00FFFFFF'
 
 // load Url
 var webview3 = ui.addWebView(0.1, 0.7, 0.8, 0.25)
