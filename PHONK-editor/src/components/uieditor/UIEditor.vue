1eb11a93c193eeda0f6deaca306b96ae15001909
Victor Diaz <victormdb@gmail.com>
2022-06-13 16:31:24 +0200

    add scoped attribute to the style

--- PHONK-editor/src/components/uieditor/UIEditor.vue
+++ PHONK-editor/src/components/uieditor/UIEditor.vue
@@ -301,7 +301,7 @@ export default {
   }
 }
 </script>
-<style lang = "less">
+<style lang = "less" scoped>
 @import (reference) '../../assets/css/variables.less';
 @import (reference) '../../assets/css/hacks.less';
 
