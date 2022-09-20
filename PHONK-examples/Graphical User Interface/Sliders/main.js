ca5c9a93aa1d6b90899b19343e50182cda353163
Victor Diaz <victormdb@gmail.com>
2022-09-20 22:10:51 +0200

    tweak slightly the formatting in new examples

--- PHONK-examples/Graphical User Interface/Sliders/main.js
+++ PHONK-examples/Graphical User Interface/Sliders/main.js
@@ -1,79 +1,79 @@
 /*
- *  Description tests every possible slider configuration.
- *  'direct' slider moves the slider to the position you clicked
- *   while 'drag' slider moves only if you move you finger.
+ * PHONK Example: Sliders
+ *
+ * Shows every possible slider configuration.
+ * 'direct' slider moves the slider to the position you clicked
+ *  while 'drag' slider moves only if you move you finger.
  */
+
+ui.addTitle(app.name)
 
 ui.addSlider(0, 0, 0.24, 0.24)
   .range(0, 100)
   .value(50)
   .text('direct')
   .mode('direct')
-  .verticalMode(true);
+  .verticalMode(true)
 
 ui.addSlider(0.25, 0, 0.24, 0.24)
   .range(0, 100)
   .value(50)
   .text('drag')
   .mode('drag')
-  .verticalMode(true);
+  .verticalMode(true)
 
 ui.addSlider(0.5, 0, 0.24, 0.24)
   .range(0, 100)
   .text('direct')
   .mode('direct')
-  .verticalMode(true);
+  .verticalMode(true)
 
 ui.addSlider(0.75, 0, 0.24, 0.24)
   .range(0, 100)
   .text('drag')
   .mode('drag')
-  .verticalMode(true);
-
-
+  .verticalMode(true)
 
 ui.addSlider(0, 0.25, 0.49, 0.24)
   .range(0, 100)
   .value(50)
   .text('direct')
-  .mode('direct');
+  .mode('direct')
 
 ui.addSlider(0.5, 0.25, 0.49, 0.24)
   .range(0, 100)
   .value(50)
   .text('drag')
-  .mode('drag');
+  .mode('drag')
 
 ui.addSlider(0, 0.5, 0.49, 0.24)
   .range(0, 100)
   .text('direct')
-  .mode('direct');
+  .mode('direct')
 
 ui.addSlider(0.5, 0.5, 0.49, 0.24)
   .range(0, 100)
   .text('drag')
-  .mode('drag');
-
-
+  .mode('drag')
 
 ui.addSlider(0, 0.75, 0.24, 0.24)
   .range(0, 100)
   .value(50)
   .mode('direct')
-  .verticalMode(true);
+  .verticalMode(true)
 
 ui.addSlider(0.25, 0.75, 0.24, 0.24)
   .range(0, 100)
   .value(50)
   .mode('drag')
-  .verticalMode(true);
+  .verticalMode(true)
 
 ui.addSlider(0.5, 0.75, 0.24, 0.24)
   .range(0, 100)
   .mode('direct')
-  .verticalMode(false);
+  .verticalMode(false)
 
 ui.addSlider(0.75, 0.75, 0.24, 0.24)
   .range(0, 100)
   .mode('drag')
-  .verticalMode(false);
+  .verticalMode(false)
